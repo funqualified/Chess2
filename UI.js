e4057173a770dd929f0c479f5b396da84176212d
markrestart <markrestart@gmail.com>
2023-06-15 15:01:27 -0400

    Random tagline

--- UI.js
+++ UI.js
@@ -30,6 +30,11 @@ window.onload = function () {
     section.appendChild(span);
     section.appendChild(document.createElement("br"));
   });
+
+  if (Math.random() < 0.5) {
+    var randomGameName = randomGameNames[Math.floor(Math.random() * randomGameNames.length)];
+    document.getElementById("game-title").innerHTML = randomGameName;
+  }
 };
 
 function quit() {
@@ -55,3 +60,62 @@ function convertToCapitalCase(str) {
 
   return str;
 }
+
+var randomGameNames = [
+  "Chess Revelations",
+  "The Elder Chess",
+  "Call of Chess: Modern Chessfare",
+  "Chessout: New Vegas",
+  "Chess Box 360",
+  "Chess 2: Pig in the City",
+  "Chess 2: Pawn in the City",
+  "Chess 2: Into Darkness",
+  "Chess-Kazooie",
+  "Chess-Tooie",
+  "Chess: 2049",
+  "Super Smash Chess Ultimate",
+  "Chess 2: The Return of Jafar",
+  "Chess 2: The Chesspire Strikes Back",
+  "Chess 2: Cruise Control",
+  "Chess 4: A New Chess",
+  "Chess Crossing",
+  "Chess 9: The Rise of Chess",
+  "Chess 2: A Tale of Two Kitties",
+  "Chess 2: The Squeakquel",
+  "2 Chess 2 Furious",
+  "Chess Ever After",
+  "Chess 2: The Legend of Curly's Gold",
+  "Chess 1 1/2",
+  "Chess 2: Die Harder",
+  "Total War: Chess 2",
+  "Chess and Chesserer",
+  "Chess 2: Judgment Day",
+  "Doki Doki Chess Club",
+  "Doki Doki Chess Club Plus",
+  "Chesses",
+  "Chesss",
+  "Chess 2: The Spy Who Shagged Me",
+  "Chess 2: The [insert piece name] Who Shagged Me",
+  "Chess 2: Dead Man's Chest",
+  "Chess 2:  Dead Man's Chess",
+  "Chess 2: Dead Man's Chest. Or perhaps Chess 2:  Dead Man's Chess",
+  "The Dark Knight",
+  "Chess II",
+  "Chess 2 and the Temple of Doom",
+  "Chess: Maverick",
+  "Chess 2: Fury Board",
+  "Chess 2: Passant Thunderdome",
+  "Chess 2: The Winter [insert piece name]",
+  "Chess: Legacy",
+  "Chess 2: Legacy",
+  "Chess 2: Your Sister is a Werewolf",
+  "Chess 2: Chess 2: Chess 2: Chess 2: Chess 2: Chess 2:",
+  "Chess-Man 2",
+  "Chess 2: Age of Ultron",
+  "Chess! Here We Go Again",
+  "Chess Returns",
+  "Chess 2: The Dark World",
+  "Chess 2: The Mysterious Island",
+  "The Chess Supremacy",
+  "Chezz II Men",
+];
